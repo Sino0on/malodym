5a430b8704c61ca0f8ef5a9a5f23cce7d8210d69
dastan <dastan.kubanuulu1@gmail.com>
2022-04-14 15:10:27 +0600

    first_commit

---
+++ shop/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404,redirect
+from .models import Category, Product
+from cart.forms import CartAddProductForm
+from .forms import ContactForm
+
+
+def product_detail(request, id, slug):
+    print(id)
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+def contact(request):
+    template_name = 'shop/product/contact.html'
+    context = {}
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = ContactForm()
+        context['form'] = form
+    return render(request, template_name, context)
+# Create your views here.

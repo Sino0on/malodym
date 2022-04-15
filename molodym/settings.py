ec3707a65303af2ec854d03f7c57d8e9fc2ea12c
Aktileksozonbaev <aktilek.sozonbaev@alatoo.edu.kg>
2022-04-16 01:20:44 +0600

    dasd

--- molodym/settings.py
+++ molodym/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'molodym/static'),
+    os.path.join(BASE_DIR, '/static/assets'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
